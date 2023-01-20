c1c8b01e8812739bfdaa391c8f1056b26b8f3689
sanam lakhotra <slakhotra@myseneca.ca>
2023-01-19 21:30:41 -0500

    updated server.js

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
+/*********************************************************************************
+ * WEB322 – Assignment 1
+ * I declare that this assignment is my own work in accordance with Seneca Academic Policy.
+ * No part of this assignment has been copied manually or electronically from any other source
+ * (including web sites) or distributed to other students.
+ *
+ * Name: sanam lakhotra Student ID: 151835212 Date: 2023/01/19
+ *
+ * Online (Cyclic) URL: https://wide-eyed-blue-tick.cyclic.app/_
+ *
+ ********************************************************************************/
 var HTTP_PORT = process.env.PORT || 8080
 var express = require('express')
 var app = express()
