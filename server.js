2b1a443eee2ca48399dfdb1a24eadd3f56afda4f
sanam lakhotra <slakhotra@myseneca.ca>
2023-01-19 21:16:41 -0500

    updated server.js

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var HTTP_PORT = process.env.PORT || 8080
 var express = require('express')
 var app = express()
 app.get('/', (req, res) => {
-  res.send('Sanam Lakhotra 1234567890')
+  res.send('Sanam Lakhotra 151835212')
 })
 app.listen(HTTP_PORT)
